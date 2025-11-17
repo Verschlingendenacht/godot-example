691e3e61f9e9c8e785888809087ea7504edab3e6
Alex <nilzon.gomez02@gmail.com>
2025-11-16 21:02:16 -0500

    final version

--- first-game/scripts/killzone.gd
+++ first-game/scripts/killzone.gd
@@ -4,8 +4,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("you died!")
+	Engine.time_scale = 0.5 #slowdown time speed of the game upon death
+	body.get_node("CollisionShape2D").queue_free() #remove player's collision node so they fall off the map
 	timer.start()
 
 
 func _on_timer_timeout() -> void: #runs when the timer ends
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
